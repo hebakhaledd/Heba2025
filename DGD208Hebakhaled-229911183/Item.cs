e2c40f0b30f8fe5c452ab408c7e78332c2a5a97a
hebakhaledd <kheba7355@gmail.com>
2025-06-10 17:21:44 +0300

    Patch 0.2
    - Added Item Class & Item Data Base
    - Adding 3 Functional Items (Food , Nap, Toy)

---
+++ DGD208Hebakhaled-229911183/Item.cs
@@ -0,0 +1,23 @@
+﻿public class Item
+{
+    public string Name { get; }
+    public PetStat AffectedStat { get; }
+    public int EffectAmount { get; }
+    public int DurationInSeconds { get; }
+
+    public Item(string name, PetStat affectedStat, int effectAmount, int durationInSeconds)
+    {
+        Name = name;
+        AffectedStat = affectedStat;
+        EffectAmount = effectAmount;
+        DurationInSeconds = durationInSeconds;
+    }
+
+    public async void ApplyTo(Pet pet)
+    {
+        Console.WriteLine($"Using {Name} on {pet.Name}...");
+        await Task.Delay(DurationInSeconds * 1000);
+        pet.IncreaseStat(AffectedStat, EffectAmount);
+        Console.WriteLine($"{pet.Name}'s {AffectedStat} increased by {EffectAmount}.");
+    }
+}
---
+++ DGD208Hebakhaled-229911183/ItemDatabase.cs
@@ -0,0 +1,21 @@
+﻿public static class ItemDatabase
+{
+    private static List<Item> items = new()
+    {
+        new Item("Food", PetStat.Hunger, 20, 2),
+        new Item("Nap", PetStat.Sleep, 25, 3),
+        new Item("Toy", PetStat.Fun, 30, 2),
+    };
+
+    private static Random random = new();
+
+    public static Item GetRandomItem()
+    {
+        return items[random.Next(items.Count)];
+    }
+
+    public static List<Item> GetAllItems()
+    {
+        return items;
+    }
+}
