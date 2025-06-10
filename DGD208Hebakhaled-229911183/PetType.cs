2e5003a7742fef10835cca23b298cade6a06d61c
hebakhaledd <kheba7355@gmail.com>
2025-06-10 17:44:45 +0300

    Patch 0.3
    -Added Pet States
    - Added Pet Types ( Dog, Cat, Bird, Rabbit, Dragon )
    - Added Event Showing The Animal States

---
+++ DGD208Hebakhaled-229911183/PetType.cs
@@ -0,0 +1,8 @@
+﻿public enum PetType
+{
+    Dog = 0,
+    Cat = 1,
+    Bird = 2,
+    Rabbit = 3,
+    Dragon = 4
+}
---
+++ DGD208Hebakhaled-229911183/StatEventArgs.cs
@@ -0,0 +1,11 @@
+﻿public class StatEventArgs : EventArgs
+{
+    public string PetName { get; }
+    public string Message { get; }
+
+    public StatEventArgs(string petName, string message)
+    {
+        PetName = petName;
+        Message = $"{petName} {message}";
+    }
+}
